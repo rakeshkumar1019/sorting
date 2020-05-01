c1e3a3d582e1dc39867b800385fd2fa1504c56e8
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2020-05-02 03:46:54 +0530

    bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void bubblesort(int * &arr,int n){
+ for(int i=0;i<n-1;i++){
+   for(int j=0;j<n-i-1;j++){
+     if(arr[j]>arr[j+1]){
+       swap(arr[j],arr[j+1]);
+     }
+   }
+ }
+
+}
+int main(){
+  int n;
+  cin>>n;
+  int *  arr=new int[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  bubblesort(arr,n);
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+}
